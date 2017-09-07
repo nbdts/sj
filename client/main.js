2f7b501944e78760b9b207f685eeb12971fafe20
RohitKuDwivedi <rdwivedi345@gmail.com>
2017-09-07 17:24:27 +0530

    first commit

--- client/main.js
+++ client/main.js
@@ -1,19 +1,16 @@
-
+//this is entry point
 
 import React from 'react';
 
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 
-import { render } from 'react-dom';
-
-
+import {render} from 'react-dom';
 
 import App from '../imports/ui/App.jsx';
 
-
-
 Meteor.startup(() => {
 
-  render(<App />, document.getElementById('render-target'));
+  render(
+    <App/>, document.getElementById('render-target'));
 
 });
