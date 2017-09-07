6b59d7f693dce419f9a26006a51cdc078ebbd628
nalakenti <nalakentipratyusha@gmail.com>
2017-09-07 22:27:30 +1200

    error resolved

--- imports/ui/pages/HomePage.js
+++ imports/ui/pages/HomePage.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import Header from '../components/Header'
+//import Header from '../components/Header'
 export default class HomePage extends Component {
   constructor() {
     super();
   }
   render(){
     return(
-       <div>
-       {Header}
-       <h1>HOme page </h1>
-       </div>
+      <div>
+      <h1>HomePage</h1>
+      </div>
     );
   }
 }
