babdc2af1cb656a99f7466acc8ec855c895f317d
RohitKuDwivedi <rdwivedi345@gmail.com>
2017-09-09 18:13:12 +0530

    for testing purpose

--- imports/ui/pages/RegistrationPage.js
+++ imports/ui/pages/RegistrationPage.js
@@ -64,35 +64,71 @@ this.state={
     return (
       <div>
 
+      <div className="page-holder">
+        <form >
+          <label className="note">fill all fields & click on Register button</label>
 
-      <body>
-      <form action={this.handleSubmit} autocomplete="off">
+          <label className="input-holder">
+            <span className="input-title">Name*</span>
+            <input id='shop_name' type="text" onChange={this.handleChange}/>
+          </label>
 
-        <fieldset>
-          <input id="first" type="text" name="first" required onChange={this.handleChange} />
-          <label for="first">First Name</label>
-          <div className="after"></div>
-        </fieldset>
+          <label className="input-holder">
+            <span className="input-title">E-mail*</span>
+            <input id="shop_email" type="text" onChange={this.handleChange}/>
+          </label>
 
-        <fieldset>
-          <input id="last" type="text" name="last" required onChange={this.handleChange} />
-          <label for="last">Last Name</label>
-          <div className="after"></div>
-        </fieldset>
+          <label className="input-holder">
+            <span className="input-title">Phone-number*</span>
+            <input id="shop_phno" type="text" onChange={this.handleChange}/>
+          </label>
 
+          <label className="input-holder">
+            <span className="input-title">GSTIN*</span>
+            <input id="shop_gstin" type="text" onChange={this.handleChange}/>
+          </label>
 
-        <fieldset>
-          <button>Submit</button>
-        </fieldset>
-      </form>
-    </body>
+          <label className="input-holder">
+            <span className="input-title">Password*</span>
+            <input id="shop_password" type="password" onChange={this.handleChange}/>
+          </label>
 
+          <label className="input-holder">
+            <span className="input-title">Confirm Password*</span>
+            <input id="" type="shop_password" onChange={this.handleChange}/>
+          </label>
 
+          <label className="note">ACCOUNT DETAILS</label>
 
+          <label className="input-holder">
+            <span className="input-title">Bank name*</span>
+            <input id="shop_bankname" type="text" onChange={this.handleChange}/>
+          </label>
 
+          <label className="input-holder">
+            <span className="input-title">account holder name*</span>
+            <input id="shop_accname" type="text" onChange={this.handleChange}/>
+          </label>
 
+          <label className="input-holder">
+            <span className="input-title">account type*</span>
+            <input id="shop_acctype" type="text" onChange={this.handleChange}/>
+          </label>
 
+          <label className="input-holder">
+            <span className="input-title">account number*</span>
+            <input id="shop_accno" type="text" onChange={this.handleChange}/>
+          </label>
 
+          <label className="input-holder">
+            <span className="input-title">account IFSC*</span>
+            <input id="shop_ifsc" type="text" onChange={this.handleChange}/>
+          </label>
+
+          <input id="" type="submit" value="Register"/>
+        </form>
+
+      </div>
 
       </div>
     );
