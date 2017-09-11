8c712e45b3557f403572c594aeede99407ac2d7f
nalakenti <nalakentipratyusha@gmail.com>
2017-09-12 06:55:36 +1200

    app route done

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import CategoryPage from "./pages/CategoryPage";
 import AdminPage from "./pages/AdminPage";
+import invoice from "./pages/invoice";
 import {Session} from 'meteor/session';
 import RegistrationPage from "./pages/RegistrationPage";
 export default class App extends Component {
@@ -33,6 +34,7 @@ export default class App extends Component {
       <Route exact path = "/admin/category" component = {AdminPage} />
       <Route exact path = "/admin/registeration" component = {AdminPage} />
       <Route exact path = "/registeration" component = {RegistrationPage} />
+      <Route exact path = "/invoice" component = {invoice} />
       <Route exact path = "/category" component = {CategoryPage} />
 
       </div>
