83a6ab576ca098ced645c26ed01c9904f2b886bd
RohitKuDwivedi <rdwivedi345@gmail.com>
2017-09-12 23:21:58 +0530

    billing system to be updated

--- imports/ui/componants/Avatar.js
+++ imports/ui/componants/Avatar.js
@@ -8,7 +8,7 @@ export default class Avatar extends Component {
     return (
       <div className="Amain">
         <div className="image">
-          <img className="dimension" src={this.props.image} alt="image"/>
+          <img className="dimension" src={this.props.image} />
         </div>
         <div className="text">
           {this.props.text}
--- imports/ui/pages/BillPage.js
+++ imports/ui/pages/BillPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {ProductApi} from '../../api/product';
 import { Tracker } from 'meteor/tracker';
+import './css/BillPage';
 import Avatar from '../componants/Avatar';
 
 export default class BillPage extends Component {
@@ -47,12 +48,18 @@ export default class BillPage extends Component {
          <ul>
            <li onClick={this.handleClick.bind(this,1)} ><a href="#" id="targeted"><Avatar  image="https://image.flaticon.com/icons/svg/167/167247.svg" text="Juice"/></a></li>
            <li onClick={this.handleClick.bind(this,2)} ><a href="#" id="targeted"><Avatar image="http://www.italysgr.com/images/icon/Pizza-icon.png" text="Pizza"/></a></li>
+           <li onClick={this.handleClick.bind(this,3)} ><a href="#" id="targeted"><Avatar image="http://icons.iconarchive.com/icons/dapino/beach/256/icecream-icon.png" text="Icecream"/></a></li>
         </ul>
        </div>
 
 
-                <div className="main">
-                  {products.map((product,i)=><div key={i}>{product.name}</div>)}
+                <div className="main box">
+                
+                  {products.map((product,i)=><div key={i}>
+                        <input  className="productmenu" type="button" value={product.name}/>
+                  </div>)}
+
+
                 </div>
      </div>
       </div>
