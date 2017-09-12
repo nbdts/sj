bde90e0096ad19676e528147a1cdd033787b1c5e
RohitKuDwivedi <rdwivedi345@gmail.com>
2017-09-12 19:31:36 +0530

    aa

---
+++ imports/ui/pages/BillPage.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+export default class AdminPage extends Component {
+  constructor() {
+    super();
+  }
+  render(){
+    return(
+      <Router>
+      <div>
+      <div className="admin-panel clearfix">
+       <div className="slidebar">
+       <div className="logo">
+           <a href='#'/>
+
+       </div>
+         <ul>
+           <li><a href="/admin/registeration" id="targeted">Add Shop</a></li>
+           <li><a href="/admin/product" id="targeted">Add Product</a></li>
+         </ul>
+       </div>
+
+
+                <div className="main">
+                  <Route exact path="/admin/registeration" component={RegistrationPage}/>
+                  <Route exact path="/admin/product" component={ProductPage}/>
+
+                </div>
+     </div>
+      </div>
+      </Router>
+    );
+  }
+}
