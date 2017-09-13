8cd064705d6555a8528c7157c51068e316e56150
RohitKuDwivedi <rdwivedi345@gmail.com>
2017-09-13 21:59:42 +0530

    done invoice

---
+++ imports/api/invoice.js
@@ -0,0 +1,27 @@
+import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+export const InvoiceApi = new Mongo.Collection('invoice');
+
+Meteor.methods({
+'invoice.insert'(name,products){
+
+  InvoiceApi.insert({
+    name,
+    products,
+    createdAt:new Date(),
+    status:1,
+   });
+},
+'invoice.remove'(shopid){
+  InvoiceApi.remove(shopid);
+},
+'invoice.update'(){
+
+},
+
+});
+if (Meteor.isServer) {
+  Meteor.publish('invoice', function userPublication(){
+    return  InvoiceApi.find();
+  });
+}
--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import {ShopApi} from '../imports/api/shops';
 import {ProductApi} from '../imports/api/product';
+import {InvoiceApi} from '../imports/api/invoice';
 Meteor.startup(() => {
   // code to run on server at startupsho
 });
