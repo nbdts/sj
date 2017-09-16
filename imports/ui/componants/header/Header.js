af02e0bc75f9da8a70a2e85f2ad093c5bca044ee
nalakenti <nalakentipratyusha@gmail.com>
2017-09-17 06:18:21 +1200

    header design done

--- imports/ui/componants/header/Header.js
+++ imports/ui/componants/header/Header.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
 import './Header.css';
-export default class Header  extends Component {
 
+export default class Header  extends Component {
   constructor() {
     super();
   }
+  // componentDidMount(){
+  //   document.getElementById('stickyContent').innerHTML = document.getElementById('main').innerHTML;
+  // }
+
   render(){
     return(
+      <header className="header-banner-top">
 
-      <div className="header-container header-body">
-      <header className="header">
-         <div className="wrap">
-          <h1 className="headerh1">SANDWICH JUNCTION</h1>
-
-        </div>
-      </header>
-      <nav className="nav">
-        <div className="wrap">
-          <ul className="navbar ul">
-            <li className="li"><a className="active header-a" href="#">About</a></li >
-            <li className="li"><a className="active header-a" href="#">Home</a></li >
-            <li className="li"><a className="active header-a" href="#">Clients</a></li>
-            <li className="li"><a className="active header-a" href="#">Contact Us</a></li>
+  <div className="main-navigation">
+    <input type="checkbox" name="mobile-menu-toggle" id="mobile-menu-toggle" className="mobile-menu-box" />
+    <nav className="horizontal-nav primary-wrapper" role='navigation'>
+      <ul>
+        <li className="active-link"><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li className="folder"> 
+          <input type="checkbox" name="folder-toggle-1" id="folder-toggle-1" className="folder-toggle-box hidden" />
+          <label for="folder-toggle-1" className="folder-toggle-label"><a>Folder</a></label>
+          <ul>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Linkus</a></li>
+            <li><a href="#">Linkorama</a></li>
+            <li><a href="#">Link</a></li>
           </ul>
-        </div>
-      </nav>
-</div>
+        </li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </nav>
+    <label for="mobile-menu-toggle" className="mobile-menu-label hidden"></label>
+  </div>
+
+</header>
     );
   }
 }
