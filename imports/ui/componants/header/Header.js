5adc2d96e00e8576c63a14d27afe7b4499cf8f45
RohitKuDwivedi <rdwivedi345@gmail.com>
2017-10-03 17:08:35 +0530

    modal and edit

--- imports/ui/componants/header/Header.js
+++ imports/ui/componants/header/Header.js
@@ -2,11 +2,32 @@ import React, {Component} from 'react';
 import './Header.css';
 import {Session} from 'meteor/session';
 import {withRouter, Redirect,} from 'react-router-dom';
+import Modal from '../modal';
+
 
 class Header extends Component {
   constructor() {
     super();
+    this.state={
+      isModalOpen: false,
+      }
+          this.closeModal = this.closeModal.bind(this);
+		this.openModal = this.openModal.bind(this);
   }
+
+  closeModal() {
+    this.setState({
+      isModalOpen: false
+    })
+  }
+
+  openModal() {
+    console.log('open');
+    this.setState({
+      isModalOpen: true
+    })
+  }
+
   handleLogout() {
     Session.clear();
     <Redirect to = "/login" />
@@ -30,7 +51,7 @@ render() {
               <a style={styles.dropDown} href="#">Report</a>
               </li>
               <li>
-              <a style={styles.dropDown} href="#">Add Product</a>
+              <button style={mainStyle.button} onClick={this.openModal} >Add Product</button>
               </li>
               <li>
                 { Session.get('shop')
@@ -50,6 +71,21 @@ render() {
         </nav>
         <label htmlFor="mobile-menu-toggle" className="mobile-menu-label hidden"></label>
       </div>
+        <Modal
+           isModalOpen={this.state.isModalOpen}
+           closeModal={this.closeModal}
+           style={modalStyle}>
+
+           <button style={{
+            ...mainStyle.button,
+            margin: 0,
+            width: 'auto',
+            marginTop: 10
+          }} onClick={this.closeModal}>Close</button>
+          <h1>
+
+       </Modal>
+
       </header>
   );
 }
@@ -59,3 +95,21 @@ export default withRouter(Header)
 const styles={
   dropDown:{fontSize:'15'}
 }
+const modalStyle = {
+	overlay: {
+		backgroundColor: 'rgba(0, 0, 0,0.5)'
+	}
+};
+
+const mainStyle = {
+	button: {
+    fontWeight:600,
+		backgroundColor: '#408cec',
+		border: 0,
+		padding: '10px 15px',
+		color: '#fff',
+		width: 150,
+		display: 'block',
+		borderRadius: 3
+	}
+};
